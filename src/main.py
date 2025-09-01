a41ec9f11a60abcbc4878bdd583d6ee2c0ebbd84
TomSmith <61518255+SeriyyKust@users.noreply.github.com>
2025-09-01 11:34:40 +0300

    Добавление секретного ключа, для проверки сканера

--- src/main.py
+++ src/main.py
@@ -1,15 +1,26 @@
+import requests
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 
 app = FastAPI()
+request_key: str = "(Cr_},%b5y@!S-#Tq|^@K74iA|PiG,4eYr@<="rl-woD%N!?]/$s]wX:_HuOmM*"
 
 
 @app.get("/test")
 async def test_handler():
-    return JSONResponse(
-        status_code=200,
-        content={
-            "message": "ok"
-        }
-    )
+    response = requests.get(url, json={"key": request_key})
+    if response.status_code == 200:
+        return JSONResponse(
+            status_code=200,
+            content={
+                "message": "ok"
+            }
+        )
+    else:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "message": "error"
+            }
+        )
